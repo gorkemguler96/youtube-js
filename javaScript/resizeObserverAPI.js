2f56973284dd113f4e061df5e0d7ccfb11747d22
slx1996 <gurkanguler73@hotmail.com>
2022-02-03 17:41:43 +0300

    youtube-js

---
+++ javaScript/resizeObserverAPI.js
@@ -0,0 +1,12 @@
+// const textArea = document.querySelector('textarea')
+//
+// const resizeObserver = new ResizeObserver(entries => {
+//     for(let entry of entries){
+//         const size = entry.contentRect.height/3-2
+//         entry.target.style.fontSize = size + 'px'
+//         entry.target.style.lineHeight = size + 'px'
+//     }
+//
+// })
+//
+// resizeObserver.observe(textArea)
